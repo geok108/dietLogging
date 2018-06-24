0111f9edd9478654aba2d1bab0a3a7f971e40bd7
gk488 <gk488@bath.ac.uk>
2018-06-25 00:53:48 +0100

    Updated add screen, added range bar

--- app/src/main/java/com/example/root/dietlogging/AddActivity.java
+++ app/src/main/java/com/example/root/dietlogging/AddActivity.java
@@ -3,11 +3,14 @@ package com.example.root.dietlogging;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.appyvet.materialrangebar.RangeBar;
+
 public class AddActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
     }
 }
