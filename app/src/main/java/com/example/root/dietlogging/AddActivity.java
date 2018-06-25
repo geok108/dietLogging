4cc2447eed09ab3840394a5549bf7dfa1bae9f45
gk488 <gk488@bath.ac.uk>
2018-06-26 00:01:09 +0100

    Updated add food screen, populated meal dropdown, updated range bar

--- app/src/main/java/com/example/root/dietlogging/AddActivity.java
+++ app/src/main/java/com/example/root/dietlogging/AddActivity.java
@@ -2,6 +2,8 @@ package com.example.root.dietlogging;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.appyvet.materialrangebar.RangeBar;
 
@@ -11,6 +13,16 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.meal_dropdown);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.meal_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
     }
 }
