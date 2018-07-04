800e977bda78e7d9e4cb06c2c51ce08020ebd97b
gk488 <gk488@bath.ac.uk>
2018-07-04 01:24:46 +0100

    Added the search view and got the query in activity

--- app/src/main/java/com/example/root/dietlogging/SearchActivity.java
+++ app/src/main/java/com/example/root/dietlogging/SearchActivity.java
@@ -1,13 +1,58 @@
 package com.example.root.dietlogging;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = findViewById(R.id.search_view);
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String q) {
+                q = searchView.getQuery().toString();
+                Log.d("QUERY: ", q);
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return false;
+            }
+
+
+
+        });
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //doMySearch(query);
+        }
+
     }
+
+
+
 }
